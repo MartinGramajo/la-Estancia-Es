247bd5fc0559f787e4d40b6244b4e958fa5bbb02
¨mago¨ <martingramajo08@gmail.com>
2023-08-10 15:04:54 -0300

    agrego boton de wsp y tel y modifico los logos

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -1,6 +1,8 @@
 import Button from "@restart/ui/esm/Button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
+import { Image } from "react-bootstrap";
 
 export default function Contacto() {
   return (
@@ -10,7 +12,28 @@ export default function Contacto() {
       </h3>
       <div className="text-center mb-4 responsive-espacios ">
         <a
-          href="https://wa.link/sjvqme"
+          href="tel:+868130872"
+          target="_blank"
+          rel="noreferrer"
+          className="numero contacto"
+        >
+          <Button
+            className="boton-whatsapp color-marron-claro-fondo ov-btn-grow-ellipse"
+            variant="light"
+          >
+            <div className="mx-3 ">
+              <h3 className="fw-bold color-blanco whatsapp-hover tamaño-bold my-2">
+                <FontAwesomeIcon className="me-2" icon={faPhone} />
+                868130872
+              </h3>
+            </div>
+          </Button>
+        </a>
+      </div>
+
+      <div className="text-center mb-4 responsive-espacios ">
+        <a
+          href="https://wa.link/u0g31u"
           target="_blank"
           rel="noreferrer"
           className="numero contacto"
@@ -22,34 +45,59 @@ export default function Contacto() {
             <div className="mx-3 ">
               <h3 className="fw-bold color-blanco whatsapp-hover tamaño-bold my-2">
                 <FontAwesomeIcon className="me-2" icon={faWhatsapp} />
-                868130872
+                747 40 91 26
               </h3>
             </div>
           </Button>
         </a>
       </div>
-      <h5 className="text-center mb-5 fs-6 ">Pedidos y recogida en el local</h5>
+
+      <h5 className="text-center mb-5  fs-6 ">
+        Pedidos y recogida en el local
+      </h5>
       <div className="text-center d-sm-block d-none">
-        <img
+        {/* <img
           className="img-fluid px-5 deliverys"
           src="https://res.cloudinary.com/laestancia/image/upload/v1647274046/editada_mycnam_ufn4qt.jpg"
           alt="deliverys"
+        /> */}
+        <div>
+          <img
+            className="img-fluid px-5 deliverys mt-2"
+            src="https://res.cloudinary.com/laestancia/image/upload/v1691687357/nuevas%20fotos/glovo_jythyc.png"
+            alt="glovo"
+          />
+          <img
+            className="img-fluid px-5 deliverys mt-4"
+            src="https://res.cloudinary.com/laestancia/image/upload/v1691686969/nuevas%20fotos/uberlogo_xyivsc.png"
+            alt="uber"
+          />
+          <img
+            className="img-fluid px-5  deliverys mt-4"
+            src="https://res.cloudinary.com/laestancia/image/upload/v1691687729/nuevas%20fotos/justeat_ienlw3.png"
+            alt="justeat"
+          />
+        </div>
+      </div>
+      <div className="text-center d-block d-sm-none">
+        <img
+          className="img-fluid px-5 pb-4 deliverys"
+          src="https://res.cloudinary.com/laestancia/image/upload/v1691687357/nuevas%20fotos/glovo_jythyc.png"
+          alt="glovo"
+        />
+      </div>
+
+      <div className="text-center d-block d-sm-none">
+        <img
+          className="img-fluid px-5 deliverys "
+          src="https://res.cloudinary.com/laestancia/image/upload/v1691686969/nuevas%20fotos/uberlogo_xyivsc.png"
+          alt="uber"
         />
       </div>
       <div className="text-center d-block d-sm-none">
         <img
-          className="img-fluid px-5 deliverys"
-          src="https://res.cloudinary.com/laestancia/image/upload/v1647274044/glovo_tdfzui_eyo0cr_o4yziv.jpg"
-          alt="glovo"
-        />
-        <img
-          className="img-fluid px-5 deliverys mt-3"
-          src="https://res.cloudinary.com/laestancia/image/upload/v1647274031/deliveroo_eexvzb_zaplke.jpg"
-          alt="deliveroo"
-        />
-        <img
-          className="img-fluid px-5 deliverys mt-3"
-          src="https://res.cloudinary.com/laestancia/image/upload/v1647274012/justeat_edwwe1_ptb9ph.jpg"
+          className="img-fluid px-5  deliverys "
+          src="https://res.cloudinary.com/laestancia/image/upload/v1691687359/nuevas%20fotos/justeat_ophtqf.png"
           alt="justeat"
         />
       </div>
